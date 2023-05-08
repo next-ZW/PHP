969eb8e654a82297967c2e23e80f61788d79f13e
next-ZW <99071376@mydavinci.nl>
2023-05-08 11:40:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HTML Formulier</title>
+</head>
+<body>
+
+ 
+
+    <form action="actie.php" method="post">
+<label for="name">Naam:</label>
+<input type="text" id="naam" name="naam"><br><br>
+
+<label for="age">Leeftijd:</label>
+<input type="leeftijd" id="age" name="leeftijd"><br><br>
+
+<input type="submit" value="Verzenden">
+</form>
+
+ 
+
+</body>
+</html>
