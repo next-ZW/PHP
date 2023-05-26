18d53544a2e553303f0347f8551bf49183cba4e8
next-ZW <99071376@mydavinci.nl>
2023-05-26 09:58:32 +0200

    Update tafel van x.php

--- module 1/tafel van x.php
+++ module 1/tafel van x.php
@@ -1,14 +1,12 @@
 <?php
 
 function tafelVan($getal) {
-    for ($i = 1; $i <= 10; $i++) {
-        $resultaat = $getal * $i;
-        echo $getal . " x " . $i . " = " . $resultaat . "<br>";
+    for ($x = 1; $x <= 10; $x++) {
+        $resultaat = $getal * $x;
+        echo $getal . " x " . $x . " = " . $resultaat . "<br>";
     }
     echo "<br>";
 }
-
-
 
 
 $waardes = array(3, 5, 6, 8, 12);
@@ -16,5 +14,13 @@ $waardes = array(3, 5, 6, 8, 12);
 foreach ($waardes as $waarde) {
     tafelVan($waarde);
 }
+
+$getal1 = mt_rand(1,10);
+$getal2 = mt_rand(1,10);
+
+echo $getal1 , " + ", $getal2 , " = ", $getal1 + $getal2 , "<br>";
+echo $getal1 , " x ", $getal2 , " = ", $getal1 * $getal2 , "<br>";
+echo $getal1 , " - ", $getal2 , " = ", $getal1 - $getal2 , "<br>";
+echo $getal1 , " : ", $getal2 , " = ", $getal1 / $getal2 , "<br>";
 
 ?>
