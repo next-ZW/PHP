f2fe8064a539ea5c33aad7256a776cc453d3f701
next-ZW <99071376@mydavinci.nl>
2023-06-19 09:48:52 +0200

    Revert "Merge branch 'main' of https://github.com/next-ZW/PHP"
    This reverts commit ade61e1467d0e80946334fbffe4c3d6e195eb037, reversing
    changes made to 69bd93f0c761467b430ed0202a830d985c43b01b.

--- module 2/welcome.php
+++ module 2/welcome.php
@@ -1,40 +1,68 @@
+<?php
+$nameErr = $emailErr = "";
+$name = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["Naam"])) {
+        $nameErr = "Naam is verplicht";
+    } else {
+        $name = test_input($_POST["Naam"]);
+    }
+    
+    if (empty($_POST["Email"])) {
+        $emailErr = "Email is verplicht";
+    } else {
+        $email = test_input($_POST["Email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "email is not a valid email address";
+        }
+    }
+    
+    if (empty($nameErr) && empty($emailErr)) {
+        echo "<!DOCTYPE html>
+                <html lang='en'>
+                <head>
+                    <meta charset='UTF-8'>
+                    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                    <title>Welcome</title>
+                    <link rel='stylesheet' href='welcome.css'>
+                </head>
+                <body>
+                    <h1>De ingevulde gegevens zijn:</h1>
+                    <p>Naam: $name</p>
+                    <p>E-mailadres: $email</p>
+                </body>
+                </html>";
+        exit();
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Form</title>
+    <link rel="stylesheet" href="welcome.css">
 </head>
 <body>
-    <?php
-    $errors = array();
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["Naam"])) {
-            $errors[] = "Naam is verplicht.";
-        }
-
-        if (empty($_POST["Email"])) {
-            $errors[] = "E-mailadres is verplicht.";
-        }
-
-        if (count($errors) == 0) {
-            echo "<h1>De ingevulde gegevens zijn:</h1>";
-            echo "<p>Naam: " . htmlspecialchars($_POST['Naam']) . "</p>";
-            echo "<p>E-mailadres: " . htmlspecialchars($_POST['Email']) . "</p>";
-        }
-    }
-    ?>
-
-    <h1>De ingevulde gegevens zijn:</h1>
-    <form action="welcome.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="Naam">Naam:</label><br>
-        <?php if (in_array("Naam is verplicht.", $errors)) { echo "<span class='error'>*</span>"; } ?>
-        <input type="text" id="Naam" name="Naam"><br>
+        <input type="text" id="Naam" name="Naam" value="<?php echo $name; ?>">
+        <span class="error">*<?php echo $nameErr;?></span><br>
         <label for="Email">Email:</label><br>
-        <?php if (in_array("E-mailadres is verplicht.", $errors)) { echo "<span class='error'>*</span>"; } ?>
-        <input type="text" id="Email" name="Email"><br>
+        <input type="text" id="Email" name="Email" value="<?php echo $email; ?>">
+        <span class="error">*<?php echo $emailErr;?></span><br>
         <input type="submit" value="Submit">
     </form>
 </body>
